d7fb881b972d389e72f939c89d64a0a62c3da665
Jesse Andringa <68082539+jesseandringa@users.noreply.github.com>
2024-05-21 12:16:10 -0600

    added change county fetch

--- chat_site/src/BotClient.js
+++ chat_site/src/BotClient.js
@@ -44,6 +44,35 @@ return fetch(url)
 export {getBotResponse};
 
 
+const changeCounty = (selectedCounty) => {
+const baseURL = 'http://localhost:3050/api'; // Base URL
+const endpoint = '/change-county'; // Endpoint
+const url = new URL(endpoint, baseURL);
+url.searchParams.append('county', selectedCounty);// Construct the full URL manually
+console.log('url: ', url);
+
+
+// Use the fetch API to send a GET request
+return fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Parse the JSON response body
+  })
+  .then(data => {
+    console.log('data: ', data);
+    return data; // Make sure to return the data here so it can be used by the caller
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+    
+    throw error; // Rethrow the error so it can be caught by the caller
+  });
+};
+
+export {changeCounty};
+
 const postEmail = (message, name,email) => {
     // Convert the user input to lowercase for easier keyword matching
 const baseURL = 'http://api:5002';
