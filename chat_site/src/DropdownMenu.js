dbafce9e47096896d336fb7b2f12548b8f2bfbf1
Jesse Andringa <68082539+jesseandringa@users.noreply.github.com>
2024-05-21 12:16:45 -0600

    Update DropdownMenu.js

--- chat_site/src/DropdownMenu.js
+++ chat_site/src/DropdownMenu.js
@@ -1,11 +1,26 @@
 // DropdownMenu.js
 import React from 'react';
 import './style/DropdownMenu.css';
+import {changeCounty} from './BotClient';
+
 const DropdownMenu = ({ selectedOption, onOptionChange }) => {
   // Function to handle option selection
   const handleOptionChange = (event) => {
     const selectedValue = event.target.value;
     onOptionChange(selectedValue); // Call the parent component's handler function
+    setTimeout(async () => {
+      try {
+        const resp = await changeCounty(selectedValue);
+        try{
+           console.log('resp: ', resp);
+        }
+        catch(error){
+            console.log('error in splitting sources')
+        }
+      } catch (error) {
+        console.error('There was an error getting the bot response:', error);
+      }
+    }, 500);
   };
   const isDefaultOptionSelected = () => {
     return selectedOption === '';
