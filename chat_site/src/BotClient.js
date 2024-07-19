a9ce838b3c63e6953e5d439a36b1d953ae78b1a8
jesseandringa <jessandringa@gmail.com>
2024-07-18 18:15:47 -0600

    server

--- chat_site/src/BotClient.js
+++ chat_site/src/BotClient.js
@@ -5,8 +5,8 @@ const getBotResponse = (message, selectedCounty, IP) => {
 // run locally
 // const baseURL = 'http://localhost:3050/api/'; // Base URL
 // run on server
-// const baseURL = 'http://3.143.1.57:3050/api/'; // Base URL
-const baseURL = 'https://munihelp.com/api/';
+const baseURL = 'http://3.143.1.57:3050/api/'; // Base URL
+// const baseURL = 'https://munihelp.com/api/';
 const endpoint = 'get-response'; // Endpoint
 const url = new URL(endpoint, baseURL);
 url.searchParams.append('message', message);
@@ -39,9 +39,9 @@ export {getBotResponse};
 const sendUserData = (ip) => {
   // Convert the user input to lowercase for easier keyword matching
 // run locally
-// const baseURL = 'http://localhost:3050/api/'; // Base URL
+const baseURL = 'http://localhost:3050/api/'; // Base URL
 // run on server
-const baseURL = 'https://munihelp.com/api/'; // Base URL
+// const baseURL = 'https://munihelp.com/api/'; // Base URL
 
 console.log('ip in sendUserData: ', ip);
 const endpoint = 'user-data'; // Endpoint
