c2131916ffc87f1e47b7940ba937e417f2c43c07
jesseandringa <jessandringa@gmail.com>
2024-09-03 14:50:04 -0600

    fixed '

--- chat_server/chat/src/postgres.py
+++ chat_server/chat/src/postgres.py
@@ -189,7 +189,7 @@ class PGDB:
             search_query = f"ip_addr = '{ip}'"
 
         user_query = f"""
-        SELECT * FROM basic_user_info WHERE {search_query};';  
+        SELECT * FROM basic_user_info WHERE {search_query};  
         """
         try:
             cursor.execute(user_query)
@@ -304,19 +304,20 @@ class PGDB:
                 return None
         else:
             # Update the user's last visited time and all other fields
-            update_query = f"""
-            UPDATE basic_user_info
-            SET last_visited = '{current_timestamp}',
-            first_name = '{given_name}',
-            last_name = '{family_name}'
-            WHERE email = '{email}';
-            """
-            try:
-                self.conn.cursor().execute(update_query)
-                self.conn.commit()
-            except Exception as e:
-                logging.error(f"Error updating data: {e}")
-                return None
+            if email:
+                update_query = f"""
+                UPDATE basic_user_info
+                SET last_visited = '{current_timestamp}',
+                first_name = '{given_name}',
+                last_name = '{family_name}'
+                WHERE email = '{email}';
+                """
+                try:
+                    self.conn.cursor().execute(update_query)
+                    self.conn.commit()
+                except Exception as e:
+                    logging.error(f"Error updating data: {e}")
+                    return None
         return True
 
     def sign_up_user(self, firstname, lastname, email, password, ip):
